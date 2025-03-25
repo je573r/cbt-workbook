7abaa854bd0c637b763ccae33e4e7f0121f8f3af
ayush <ayushagrawal@myyahoo.com>
2025-03-25 04:16:57 +0000

    added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const authenticateJWT = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).render("signin", { title: "Signin", message: "Please log in to continue." });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).render("signin", { title: "Signin", message: "Session expired. Please sign in again." });
+        }
+        req.user = user;
+        next();
+    });
+};
