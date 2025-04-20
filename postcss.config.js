e821e91536eece1632d9b8608da3798b186d37f3
ayush <ayushagrawal@myyahoo.com>
2025-04-20 01:10:43 +0000

    switched to liquidjs

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    plugins: [
-      require('@tailwindcss/postcss'),
-      require('autoprefixer'),
-    ],
-  };
-  
+  plugins: [
+    require('@tailwindcss/postcss'),
+  ],
+};
